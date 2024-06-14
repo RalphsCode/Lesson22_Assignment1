26ad534d4326da261e2eb2d2f666660bb7688a05
Ralph's Code <RalphsCode@gmail.com>
2024-06-13 22:16:08 -0700

    Wrote code to edit and delete a user. Also added some bootstrap.

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 """Blogly application."""
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 from models import db, connect_db, User
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)  # creating an instance of the Flask Class
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Ponderosa@localhost/blogly'
+app.config['SQLALCHEMY_RECORD_QUERIES'] = True
 app.config['SQLALCHEMY_ECHO'] = True
 
 with app.app_context():
@@ -46,9 +47,29 @@ def user_detail(user_id):
     return render_template('user.html', user=user)
 
 @app.route('/users/<int:user_id>/edit', methods=['GET', 'POST'])
-def user_edit(user_id):
+def edit_user(user_id):
+    user = User.query.get_or_404(user_id)
     if request.method == 'GET':
-        user = User.query.get_or_404(user_id)
         return render_template('edit_user.html', user=user)
     else:
-        return render_template('edit_user.html', user_id=user_id)
+        first_name = request.form.get('first_name')
+        last_name  = request.form.get('last_name')
+        image_url = request.form.get('image_url')
+        # If 1 or more fields are updated, update the record
+        if first_name != '':
+            user.first_name = first_name
+        if last_name != '':
+            user.last_name = last_name
+        if image_url != '':
+            user.image_url = image_url
+        flash('+++ User has been updated +++')
+        db.session.add(user)
+        db.session.commit()
+        return render_template('user.html', user=user)
+    
+@app.route('/users/<int:user_id>/delete')
+def delete_user(user_id):
+    User.query.filter_by(id = user_id).delete()
+    db.session.commit()
+    flash(' >>> User has been deleted <<<')
+    return redirect('/users')
